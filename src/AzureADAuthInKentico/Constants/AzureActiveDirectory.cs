9d5ed43bfdbb51d6cddc925f7e6a14982bffa045
Kate Orlova <kate.orlova@deleteagency.com>
2019-02-24 15:55:00 +0000

    Added a GraphResourceUri to constant definition
    Graph API provides programmatic access to Azure AD through OData REST API endpoints

--- src/AzureADAuthInKentico/Constants/AzureActiveDirectory.cs
+++ src/AzureADAuthInKentico/Constants/AzureActiveDirectory.cs
@@ -3,5 +3,6 @@
     public static class AzureActiveDirectory
     {
         public const string Authority = "https://login.microsoftonline.com/{0}";
+        public const string GraphResourceUri = "https://graph.windows.net/{0}";
     }
 }
