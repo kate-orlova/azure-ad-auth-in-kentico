7afba3688148dec73b4326dbc8329dccbdfdfae9
Kate Orlova <kate.orlova@deleteagency.com>
2019-02-24 16:13:20 +0000

    Specified a ClientID property via a custom website setting

--- src/AzureADAuthInKentico/Settings/AzureActiveDirectory.cs
+++ src/AzureADAuthInKentico/Settings/AzureActiveDirectory.cs
@@ -1,8 +1,11 @@
-﻿namespace AzureADAuthInKentico.Constants
+﻿using CMS.DataEngine;
+
+namespace AzureADAuthInKentico.Constants
 {
     public static class AzureActiveDirectory
     {
         public const string Authority = "https://login.microsoftonline.com/{0}";
         public const string GraphResourceUri = "https://graph.windows.net/{0}";
+        public static string ClientID => SettingsKeyInfoProvider.GetValue("AzureClientID");
     }
 }
