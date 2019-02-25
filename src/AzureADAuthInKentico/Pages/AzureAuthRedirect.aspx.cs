4a73c13be98f2203aeeb7bbbaa3d36eb13eaea1a
Kate Orlova <kate.orlova@deleteagency.com>
2019-02-25 08:27:42 +0000

    Created an AzureAuthRedirect page

---
+++ src/AzureADAuthInKentico/Pages/AzureAuthRedirect.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureADAuthInKentico.Pages
+{
+    public partial class AzureAuthRedirect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
