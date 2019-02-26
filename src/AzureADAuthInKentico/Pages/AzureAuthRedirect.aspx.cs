5d3d69d795a08367539a5ff7f33f49513ad61e82
Kate Orlova <kate.orlova@deleteagency.com>
2019-02-26 19:09:23 +0000

    Defined an auth result of acquiring a token from the authority

--- src/AzureADAuthInKentico/Pages/AzureAuthRedirect.aspx.cs
+++ src/AzureADAuthInKentico/Pages/AzureAuthRedirect.aspx.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Web;
+using CMS.Helpers;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace AzureADAuthInKentico.Pages
 {
     public partial class AzureAuthRedirect : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected async void Page_Load(object sender, EventArgs e)
         {
             ClientCredential credential = new ClientCredential(Constants.AzureActiveDirectory.ClientId,
                 Constants.AzureActiveDirectory.AppKey);
             var authContext =
                 new AuthenticationContext(string.Format(Constants.AzureActiveDirectory.Authority,
                     Constants.AzureActiveDirectory.TenantId));
+            var code = ValidationHelper.GetString(HttpContext.Current.Request.QueryString["code"], string.Empty);
+            AuthenticationResult result =
+                await
+                    authContext.AcquireTokenByAuthorizationCodeAsync(code,
+                        new Uri(Request.Url.GetLeftPart(UriPartial.Path)), credential,
+                        string.Format(Constants.AzureActiveDirectory.GraphResourceUri, ""));
+
 
         }
     }
