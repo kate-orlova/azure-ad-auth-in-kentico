50515514bf7a0dcde55d6ff48686ebb7509f53bb
Kate Orlova <kate.orlova@deleteagency.com>
2019-02-28 20:53:20 +0000

    Implemented GetReturnUrl() extension

--- src/AzureADAuthInKentico/Extensions/UrlHelperExtensions.cs
+++ src/AzureADAuthInKentico/Extensions/UrlHelperExtensions.cs
@@ -1,6 +1,31 @@
-﻿namespace AzureADAuthInKentico.Extensions
+﻿using System.Net.Http;
+using System.Web;
+
+namespace AzureADAuthInKentico.Extensions
 {
     public static class UrlHelperExtensions
     {
+        public static string GetReturnUrl(this HttpRequest request, string defaultUrl = "/")
+        {
+            var queryReturnUrl = request?.QueryString["returnurl"];
+            if (queryReturnUrl != null)
+            {
+                return queryReturnUrl;
+            }
+
+            if (request?.UrlReferrer == null)
+            {
+                return defaultUrl;
+            }
+
+            var refererReturnUrl = HttpContext.Current.Request.UrlReferrer;
+            var refererQuerystringReturnUrl = refererReturnUrl.ParseQueryString()["returnurl"];
+            if (refererQuerystringReturnUrl.IsEmpty())
+            {
+                return defaultUrl;
+            }
+
+            return refererQuerystringReturnUrl;
+        }
     }
 }
