d230dca47b72e7ee4c67925372fe46488c2ac6d2
Kate Orlova <kate.orlova@deleteagency.com>
2019-03-01 19:57:14 +0000

    Implemented TreeNodeExtensions() method

--- src/AzureADAuthInKentico/Extensions/TreeNodeExtensions.cs
+++ src/AzureADAuthInKentico/Extensions/TreeNodeExtensions.cs
@@ -1,6 +1,18 @@
-﻿namespace AzureADAuthInKentico.Extensions
+﻿using CMS.DocumentEngine;
+using CMS.DocumentEngine.Web.UI;
+using CMS.SiteProvider;
+
+namespace AzureADAuthInKentico.Extensions
 {
     public static class TreeNodeExtensions
     {
+        public static string GetRelativeUrl(this TreeNode node)
+        {
+            var siteName = SiteContext.CurrentSite.DomainName;
+            var url = TransformationHelper.HelperObject.GetDocumentUrl(siteName, node.NodeAliasPath,
+                node.DocumentUrlPath);
+
+            return url;
+        }
     }
 }
