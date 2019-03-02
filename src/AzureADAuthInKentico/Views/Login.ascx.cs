f4fffda4dcc588280d60e46ce29e759dd8e4fbbf
Kate Orlova <kate.orlova@deleteagency.com>
2019-03-02 13:11:25 +0000

    Added a Login control

---
+++ src/AzureADAuthInKentico/Views/Login.ascx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureADAuthInKentico.Views
+{
+    public partial class Login : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
