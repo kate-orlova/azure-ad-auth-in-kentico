7d49aecf32f909c3d0f87039d23a57a74b42b7a7
Kate Orlova <kate.orlova@deleteagency.com>
2019-03-03 09:35:42 +0000

    Defined a KenticoRedirectPage for auth redirect back to Kentico

--- src/AzureADAuthInKentico/Settings/AzureActiveDirectory.cs
+++ src/AzureADAuthInKentico/Settings/AzureActiveDirectory.cs
@@ -9,6 +9,7 @@ namespace AzureADAuthInKentico.Constants
     {
         public const string Authority = "https://login.microsoftonline.com/{0}";
         public const string GraphResourceUri = "https://graph.windows.net/{0}";
+        public const string KenticoRedirectPage = "/Pages/AzureAuthRedirect.aspx";
         public static string ClientId => SettingsKeyInfoProvider.GetValue("AzureClientId");
         public static string AppKey => SettingsKeyInfoProvider.GetValue("AzureApplicationKey");
         public static string TenantId => SettingsKeyInfoProvider.GetValue("AzureTenantId");
